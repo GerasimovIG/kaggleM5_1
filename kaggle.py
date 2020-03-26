bcaae56f946fc4e4e43cb3bfb98c7ff3e036d668
Ilia Gerasimov <gerasimovig1993@mail.ru>
2020-03-26 17:07:10 +0300

    did until Tensor Flow

--- kaggle.py
+++ kaggle.py
@@ -37,8 +37,168 @@ def reduce_mem_usage(df, verbose=True):
     if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
     return df
 
-path = "../kaggleM5_data"
+path = r"..\kaggleM5_data"
 
-calendar = pd.read_csv(os.path.join(path, "calendar.csv"))
-selling_prices = pd.read_csv(os.path.join(path, "sell_prices.csv"))
-sample_submission = pd.read_csv(os.path.join(path, "sample_submission.csv"))
+calendar =  pd.read_csv(os.path.join(path, r"calendar.csv"))
+selling_prices = pd.read_csv(os.path.join(path, r"sell_prices.csv"))
+sample_submission = pd.read_csv(os.path.join(path, r"sample_submission.csv"))
+sales = pd.read_csv(os.path.join(path, r"sales_train_validation.csv"))
+
+
+for i, var in enumerate(["year", "weekday", "month", "event_name_1", "event_name_2", 
+                         "event_type_1", "event_type_2", "snap_CA", "snap_TX", "snap_WI"]):
+    plt.figure()
+    g = sns.countplot(calendar[var])
+    g.set_xticklabels(g.get_xticklabels(), rotation=45)
+    g.set_title(var)
+    
+    
+from sklearn.preprocessing import OrdinalEncoder
+def prep_calendar(df):
+    df = df.drop(["date", "weekday"], axis=1)
+    df = df.assign(d = df.d.str[2:].astype(int))
+    df = df.fillna("missing")
+    cols = list(set(df.columns) - {"wm_yr_wk", "d"})
+    df[cols] = OrdinalEncoder(dtype="int").fit_transform(df[cols])
+    df = reduce_mem_usage(df)
+    return df
+
+calendar = prep_calendar(calendar)
+calendar.head()
+selling_prices.info()
+
+
+
+
+def prep_selling_prices(df):
+    gr = df.groupby(["store_id", "item_id"])["sell_price"]
+    df["sell_price_rel_diff"] = gr.pct_change()
+    df["sell_price_roll_sd7"] = gr.transform(lambda x: x.rolling(7).std())
+    df["sell_price_cumrel"] = (gr.shift(0) - gr.cummin()) / (1 + gr.cummax() - gr.cummin())
+    df = reduce_mem_usage(df)
+    return df
+
+selling_prices = prep_selling_prices(selling_prices)
+
+selling_prices.tail()
+
+#######sales
+for i, var in enumerate(["state_id", "store_id", "cat_id", "dept_id"]):
+    plt.figure()
+    g = sns.countplot(sales[var])
+    g.set_xticklabels(g.get_xticklabels(), rotation=45)
+    g.set_title(var)
+
+sales.item_id.value_counts()
+
+
+def reshape_sales(df, drop_d = None):
+    if drop_d is not None:
+        df = df.drop(["d_" + str(i + 1) for i in range(drop_d)], axis=1)
+    df = df.assign(id=df.id.str.replace("_validation", ""))
+    df = df.reindex(columns=df.columns.tolist() + ["d_" + str(1913 + i + 1) for i in range(2 * 28)])
+    df = df.melt(id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
+                 var_name='d', value_name='demand')
+    df = df.assign(d=df.d.str[2:].astype("int16"))
+    return df
+
+sales = reshape_sales(sales, 1000)
+
+sns.countplot(sales["demand"][sales["demand"] <= 10]);
+
+def prep_sales(df):
+    df['lag_t28'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28))
+    df['rolling_mean_t7'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(7).mean())
+    df['rolling_mean_t30'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(30).mean())
+    df['rolling_mean_t60'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(60).mean())
+    df['rolling_mean_t90'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(90).mean())
+    df['rolling_mean_t180'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(180).mean())
+    df['rolling_std_t7'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(7).std())
+    df['rolling_std_t30'] = df.groupby(['id'])['demand'].transform(lambda x: x.shift(28).rolling(30).std())
+
+    # Remove rows with NAs except for submission rows. rolling_mean_t180 was selected as it produces most missings
+    df = df[(df.d >= 1914) | (pd.notna(df.rolling_mean_t180))]
+    df = reduce_mem_usage(df)
+
+    return df
+
+sales = prep_sales(sales)
+sales.head()
+
+sales = sales.merge(calendar, how="left", on="d")
+gc.collect()
+sales.head()
+
+sales = sales.merge(selling_prices, how="left", on=["wm_yr_wk", "store_id", "item_id"])
+sales.drop(["wm_yr_wk"], axis=1, inplace=True)
+gc.collect()
+sales.head()
+
+del selling_prices
+sales.info()
+
+###prepare date for keras
+cat_id_cols = ["item_id", "dept_id", "store_id", "cat_id", "state_id"]
+cat_cols = cat_id_cols + ["wday", "month", "year", "event_name_1", 
+                          "event_type_1", "event_name_2", "event_type_2"]
+
+# In loop to minimize memory use
+for i, v in tqdm(enumerate(cat_id_cols)):
+    sales[v] = OrdinalEncoder(dtype="int").fit_transform(sales[[v]])
+
+sales = reduce_mem_usage(sales)
+sales.head()
+gc.collect()
+
+num_cols = ["sell_price", "sell_price_rel_diff", "sell_price_roll_sd7", "sell_price_cumrel",
+            "lag_t28", "rolling_mean_t7", "rolling_mean_t30", "rolling_mean_t60", 
+            "rolling_mean_t90", "rolling_mean_t180", "rolling_std_t7", "rolling_std_t30"]
+bool_cols = ["snap_CA", "snap_TX", "snap_WI"]
+dense_cols = num_cols + bool_cols
+
+# Need to do column by column due to memory constraints
+for i, v in tqdm(enumerate(num_cols)):
+    sales[v] = sales[v].fillna(sales[v].median())
+    
+sales.head()
+
+test = sales[sales.d >= 1914]
+test = test.assign(id=test.id + "_" + np.where(test.d <= 1941, "validation", "evaluation"),
+                   F="F" + (test.d - 1913 - 28 * (test.d > 1941)).astype("str"))
+test.head()
+gc.collect()
+
+
+# Input dict for training with a dense array and separate inputs for each embedding input
+def make_X(df):
+    X = {"dense1": df[dense_cols].to_numpy()}
+    for i, v in enumerate(cat_cols):
+        X[v] = df[[v]].to_numpy()
+    return X
+
+# Submission data
+X_test = make_X(test)
+
+# One month of validation data
+flag = (sales.d < 1914) & (sales.d >= 1914 - 28)
+valid = (make_X(sales[flag]),
+         sales["demand"][flag])
+
+# Rest is used for training
+flag = sales.d < 1914 - 28
+X_train = make_X(sales[flag])
+y_train = sales["demand"][flag]
+                             
+del sales, flag
+gc.collect()
+
+np.unique(X_train["state_id"])
+
+
+#model
+import tensorflow as tf
+import tensorflow.keras as keras
+
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Dense, Input, Embedding, Dropout, concatenate, Flatten
+from tensorflow.keras.models import Model
